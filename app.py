07a5dccc758a17618273c166104ecf197051c16b
Bao Pham <90734895+baoga313@users.noreply.github.com>
2026-03-30 19:13:00 -0400

    Update api to frontend, css using react with vite, get user real location

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
 from services.air_quality import get_current_pm25
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def home():
     return 'Hello, World!'
