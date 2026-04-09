db520d968737504a0d3aec8ea27b8a54736ee0a4
Bao Pham <90734895+baoga313@users.noreply.github.com>
2026-04-08 22:27:03 -0400

    update email subscribe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,10 @@ function App() {
   const [pm25, setPm25] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [email, setEmail] = useState("");
+  const [subMessage, setSubMessage] = useState("");
   console.log("Rendering... pm25 =", pm25);
-
+  // Call api to check the current pm 2.5 based on user's location
   function checkAirQuality() {
     setLoading(true);
     setError(null);
@@ -25,13 +27,69 @@ function App() {
           setPm25(data.pm2_5);
           setLoading(false);
         });
+      (err) => {
+        console.log("Location error:", err);
+        setError("Location access denied");
+        setLoading(false);
+      };
     });
   }
-  (err) => {
-    console.log("Location error:", err);
-    setError("Location access denied");
-    setLoading(false);
-  };
+  //check the level of air quality based on pm 2.5
+  function getLevel(value) {
+    if (value < 0) {
+      return "Unknown value";
+    } else if (value <= 12) {
+      return "Good";
+    } else if (value <= 35.4) {
+      return "Moderate";
+    } else if (value <= 55.4) {
+      return "Unhealthy for Sensitive Groups";
+    } else if (value <= 150.4) {
+      return "Unhealthy";
+    } else {
+      return "Very Unhealthy";
+    }
+  }
+  //get the class name for different level to display different color in css
+  function getLevelClass(value) {
+    if (value < 0) {
+      return "level-unknown";
+    } else if (value <= 12) {
+      return "level-good";
+    } else if (value <= 35.4) {
+      return "level-moderate";
+    } else if (value <= 55.4) {
+      return "level-unhealthy-sensitive";
+    } else if (value <= 150.4) {
+      return "level-unhealthy";
+    } else {
+      return "level-very-unhealthy";
+    }
+  }
+
+  function handleSubscribe() {
+    if (!email) {
+      setSubMessage("Please enter your email!");
+      return;
+    }
+    //ask for user location
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        fetch("http://127.0.0.1:5000/api/subscribe", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email: email, lat: lat, lon: lon }),
+        })
+          .then((response) => response.json())
+          .then((data) => setSubMessage(data.message));
+      },
+      (err) => {
+        setSubMessage("Locaton access denied");
+      },
+    );
+  }
   return (
     <div className="container">
       <div className="header">
@@ -53,40 +111,26 @@ function App() {
           </p>
         </div>
       )}
+      <div className="subscribe-card">
+        <h2 className="get-email"> Do you want to get PM2.5 alert?</h2>
+        <input
+          className="user-input"
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <button
+          className="subscribe-btn"
+          type="button"
+          onClick={handleSubscribe}
+        >
+          Subscribe
+        </button>
+        {subMessage && <p className="sub-message">{subMessage}</p>}
+      </div>
     </div>
   );
-
-  function getLevel(value) {
-    if (value < 0) {
-      return "Unknown value";
-    } else if (value <= 12) {
-      return "Good";
-    } else if (value <= 35.4) {
-      return "Moderate";
-    } else if (value <= 55.4) {
-      return "Unhealthy for Sensitive Groups";
-    } else if (value <= 150.4) {
-      return "Unhealthy";
-    } else {
-      return "Very Unhealthy";
-    }
-  }
-
-  function getLevelClass(value) {
-    if (value < 0) {
-      return "level-unknown";
-    } else if (value <= 12) {
-      return "level-good";
-    } else if (value <= 35.4) {
-      return "level-moderate";
-    } else if (value <= 55.4) {
-      return "level-unhealthy-sensitive";
-    } else if (value <= 150.4) {
-      return "level-unhealthy";
-    } else {
-      return "level-very-unhealthy";
-    }
-  }
 }
 
 export default App;
