93f9334ab47870532cdaa105eae1da1d17761218
Bao Pham <90734895+baoga313@users.noreply.github.com>
2026-08-24 20:50:54 -0400

    set up email and information to sent

---
+++ services/email_service.py
@@ -0,0 +1,34 @@
+import os
+import smtplib
+from email.message import EmailMessage
+from  dotenv import load_dotenv
+
+
+load_dotenv()
+GMAIL_USER = os.getenv("GMAIL_USER")
+GMAIL_PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
+
+def send_alert(to_email, predicted_value, threshold, city):
+    msg = EmailMessage()
+    msg["Subject"] = f"PM2.5 Alert: {predicted_value} µg/m³ forecast in {city} "
+    msg["From"] = GMAIL_USER
+    msg["To"] = to_email
+
+    msg.set_content(
+        f"Air quality alert for {city}\n\n"
+        f"Forecast PM2.5 for the next hour: {predicted_value} µg/m³\n"
+        f"Your alert threshold: {threshold}µg/m³\n\n"
+        f"Consider limiting prolonged outdoor activity, and keep windows "
+        f"closed if levels stay elevated.\n\n"
+        f"This is a forecast from a model trained on industrial sensor data "
+        f"from Ploiesti, Romania. Predictions for other regions are estimates.\n"
+        f"Data by Open-Meteo.com (CC BY 4.0)\n"
+    )
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, timeout=10) as server:
+            server.login(GMAIL_USER, GMAIL_PASSWORD)
+            server.send_message(msg)
+        return True
+    except Exception as e:
+        print(f"Failed to sent to {to_email}: {e}")
+        return False
